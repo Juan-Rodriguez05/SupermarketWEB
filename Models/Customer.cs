2d3133b2ccd346f19074286af6cd074d12b54f63
Juan-Rodriguez05 <modzjuanes94@gmail.com>
2025-05-09 12:50:22 -0500

    Model, Bd and Index Customer

---
+++ Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace SupermarketWEB.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public DateTime? Birthday { get; set; }
+        public string? Address { get; set; }
+        public string? Phone_number { get; set; }
+        public string? Email { get; set; }
+    }
+}
